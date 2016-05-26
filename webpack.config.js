fab2e3b58bb962248be245e9cd9fde0dc98a623c
Eric McKay <eric@andcode.co>
2016-05-26 16:30:53 +0000

    change to cheap source map

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         "path": path.join(__dirname, 'build'),
         "filename": "bundle.js"
     },
-    devtool: "source-map",
+    devtool: "cheap-source-map",
     "module": {
         "loaders": [
             {
