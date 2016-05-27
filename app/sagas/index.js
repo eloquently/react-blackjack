72733d943af146da6960d4efc2c0a246d37ccb16
Eric McKay <eric@andcode.co>
2016-05-27 19:56:28 +0000

    onStand saga

--- app/sagas/index.js
+++ app/sagas/index.js
@@ -1,8 +1,24 @@
 import 'babel-polyfill';
-import { takeLatest } from 'redux-saga';
+import { takeLatest, delay } from 'redux-saga';
+import { select, put, call } from 'redux-saga/effects';
+import { score } from '../lib/cards';
+import { dealToDealer, determineWinner } from '../action_creators';
+
+const getDealerHand = (state) => state.get('dealerHand');
 
 export function* onStand() {
-    console.log("stand action");
+    let dealerHand;
+    while(true) {
+        yield put(dealToDealer());
+        dealerHand = yield select(getDealerHand);
+        if(score(dealerHand) >= 17) {
+            break;
+        }
+        else {
+            yield call(delay, 750);
+        }
+    }
+    yield put(determineWinner());
 }
 
 export default function*() {
