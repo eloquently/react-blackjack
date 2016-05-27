76431a5e8e81611b644691d645be568fcbdfbba9
Eric McKay <eric@andcode.co>
2016-05-27 16:12:43 +0000

    install saga middleware

--- app/index.js
+++ app/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from './components/app.js';
-import {createStore} from 'redux';
+import { createStore, applyMiddleware,
+         compose } from 'redux';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from './reducer';
-import { setupGame, setRecord } from '../app/action_creators';
+import { setupGame,
+         setRecord } from '../app/action_creators';
+import watchActions from './sagas/index';
 
 require('./css/main.scss');
 
-let store = createStore(reducer, undefined, window.devToolsExtension ? window.devToolsExtension() : undefined);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducer, undefined, compose(
+    applyMiddleware(sagaMiddleware),
+    window.devToolsExtension ? 
+        window.devToolsExtension() : f => f
+));
+
+sagaMiddleware.run(watchActions);
 
 store.dispatch(setRecord(0, 0));
 store.dispatch(setupGame());
