800bd70329a9b3552e93b5c3c8cd323587bf4639
Eric McKay <eric@andcode.co>
2016-05-27 21:14:40 +0000

    finish onStand saga tests

--- test/sagas/index_spec.js
+++ test/sagas/index_spec.js
@@ -1,5 +1,19 @@
 import { expect } from 'chai';
 import watchActions from '../../app/sagas/index';
+import proxyquire from 'proxyquire';
+import sinon from 'sinon';
+
+const actionType = (next) => {
+    return next.value.PUT.action.type;
+};
+
+const selectorName = (next) => {
+    return next.value.SELECT.selector.name;
+};
+
+const callFnName = (next) => {
+    return next.value.CALL.fn.name;
+};
 
 describe('sagas', () => {
     describe('watchActions()', () => {
@@ -9,6 +23,67 @@ describe('sagas', () => {
         it('yields takeLatest', () => {
             expect(next.value.name).to
                 .eq('takeLatest(STAND, onStand)');
+        });
+    });
+    
+    describe('onStand()', () => {
+        const cardUtils = {};
+        const stubbedSagas = proxyquire(
+            '../../app/sagas/index',
+            { '../lib/cards' : cardUtils }
+        );
+        
+        let generator;
+        
+        beforeEach( () => {
+            cardUtils.score = sinon.stub();
+            generator = stubbedSagas.onStand();
+        });
+        
+        context('when dealer does not draw' , () => {
+            it('yields correct effects', () => {
+                cardUtils.score.returns(21);
+                
+                expect(actionType(generator.next())).to
+                    .eq('DEAL_TO_DEALER');
+                
+                expect(selectorName(generator.next())).to
+                    .eq('getDealerHand');
+                    
+                expect(actionType(generator.next())).to
+                    .eq('DETERMINE_WINNER');
+                
+                expect(generator.next().done).to
+                    .eq(true);
+            });
+        });
+        
+        context('when dealer draws' , () => {
+            it('yields correct effects', () => {
+                cardUtils.score.onCall(0).returns(10);
+                cardUtils.score.onCall(1).returns(21);
+
+                expect(actionType(generator.next())).to
+                    .eq('DEAL_TO_DEALER');
+
+                expect(selectorName(generator.next())).to
+                    .eq('getDealerHand');
+
+                expect(callFnName(generator.next())).to
+                    .eq('delay');
+
+                expect(actionType(generator.next())).to
+                    .eq('DEAL_TO_DEALER');
+
+                expect(selectorName(generator.next())).to
+                    .eq('getDealerHand');
+                    
+                expect(actionType(generator.next())).to
+                    .eq('DETERMINE_WINNER');
+
+                expect(generator.next().done).to
+                    .eq(true);
+            });
         });
     });
 });
