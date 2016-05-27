7234fb107f78f19e23e66ddea20836c3e998b85f
Eric McKay <eric@andcode.co>
2016-05-27 16:05:51 +0000

    takeLatest stand saga and test

--- app/sagas/index.js
+++ app/sagas/index.js
@@ -1,11 +1,10 @@
 import 'babel-polyfill';
+import { takeLatest } from 'redux-saga';
+
+export function* onStand() {
+    console.log("stand action");
+}
 
 export default function*() {
-    let i = 0;
-    yield 'start';
-    while(true) {
-        yield i;
-        i++;
-        yield 'end loop';
-    }
+    yield takeLatest('STAND', onStand);
 }
--- test/sagas/index_spec.js
+++ test/sagas/index_spec.js
@@ -3,17 +3,12 @@ import watchActions from '../../app/sagas/index';
 
 describe('sagas', () => {
     describe('watchActions()', () => {
-        it('counts up', () => {
-            const generator = watchActions();
-            let i;
-            i = generator.next().value;
-            expect(i).to.eq('start');
-            i = generator.next().value;
-            expect(i).to.eq(0);
-            i = generator.next().value;
-            expect(i).to.eq('end loop');
-            i = generator.next().value;
-            expect(i).to.eq(1);
+        const generator = watchActions();
+        const next = generator.next();
+        
+        it('yields takeLatest', () => {
+            expect(next.value.name).to
+                .eq('takeLatest(STAND, onStand)');
         });
     });
 });
